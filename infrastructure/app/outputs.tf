4a98c8059358393909f922eaff2004fbc1561bb6
Your Name <emanbeltre726@gmail.com>
2026-08-06 02:53:07 -0400

    Added independent frontend update job. Added GitHub Actions integration provider for automated repo variables.

--- infrastructure/app/outputs.tf
+++ infrastructure/app/outputs.tf
@@ -6,16 +6,6 @@ output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.site.id
 }
 
-output "api_gateway_url" {
-  value = aws_apigatewayv2_api.contact_form.api_endpoint
-}
-
-output "certificate_validation_records" {
-  value = [
-    for dvo in aws_acm_certificate.site.domain_validation_options : {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  ]
+output "frontend_deploy_role_arn" {
+  value = aws_iam_role.frontend_deploy.arn
 }
